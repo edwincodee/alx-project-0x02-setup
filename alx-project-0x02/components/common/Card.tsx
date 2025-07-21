4e734a9df31fb182e95a08e7e50d08e9cec6c800
edwincodee <edwiinnemmanuell@gmail.com>
2025-07-21 02:59:25 +0100

    created a reuseable card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,11 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div>
+      <h3>Welcome to next js project</h3>
+    </div>
+  );
+};
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,10 @@
+import Card from "@/components/common/Card";
+
 const Home: React.FC = () => {
   return (
     <div>
       <h3>Welcome to home page</h3>
+      <Card title="Barcelona Fc" content="mes un club" />
     </div>
   );
 };
